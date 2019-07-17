df689458ccf093719e7e1fe64ba7d8081d6b41b8
jinsong <1156083136@qq.com>
2019-07-17 23:13:48 +0800

    Serializ added

---
+++ src/main/java/com/jinsong/SerializDemo.java
@@ -0,0 +1,39 @@
+package com.jinsong;
+
+
+import java.io.*;
+
+public class SerializDemo {
+    private static final File SAVE_FILE = new File("D:"+File.separator+"serializaObject");
+
+    public static void main(String[] args) throws Exception{
+        saveObject(new Person("靳松",25));
+        System.out.println(loadObject());
+    }
+
+    public static void saveObject(Object obj) throws Exception{
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(SAVE_FILE));
+        oos.writeObject(obj);
+        oos.close();
+    }
+    public static Object loadObject() throws Exception{
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(SAVE_FILE));
+        Object obj = ois.readObject();
+        ois.close();
+        return obj;
+    }
+
+}
+@SuppressWarnings("serial")
+class Person implements Serializable{
+    private transient String name;
+    private int age;
+    Person(String a,int b){
+        this.name = a;
+        this.age = b;
+    }
+    @Override
+    public String toString(){
+        return this.name+this.age;
+    }
+}
