76718945994e0e95996e08604b9ee0e75650d34e
jinsong <jinsong@jiedaibao.com>
2019-07-25 16:30:20 +0800

    werf

---
+++ shell/Spark.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+spark-submit --master yarn \
+--name "demo" \
+--class com.jinsong.sparkcodes.RddOperate \
+/Users/jinsong/GitProject/MyDemo/target/MyDemo-1.0-SNAPSHOT.jar
+
+spark-submit --master local \
+--name "demo" \
+--class com.jinsong.sparkcodes.RddOperate \
+/Users/jinsong/GitProject/MyDemo/target/MyDemo-1.0-SNAPSHOT.jar
+
+spark-submit --master spark://localhost:7070 \
+--name "demo" \
+--class com.jinsong.sparkcodes.RddOperate \
+/Users/jinsong/GitProject/MyDemo/target/MyDemo-1.0-SNAPSHOT.jar
