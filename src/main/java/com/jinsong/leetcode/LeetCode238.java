8179f203ab094c1972188b8dbe3892cd5423cccc
jinsong <jinsong@jiedaibao.com>
2019-08-28 15:02:59 +0800

    werf

--- src/main/java/com/jinsong/leetcode/LeetCode238.java
+++ src/main/java/com/jinsong/leetcode/LeetCode238.java
@@ -30,11 +30,45 @@ public class LeetCode238 {
 
         return result;
     }
+
+    public static int[] productExceptSelf2(int[] nums) {
+
+        int nlen = nums.length;
+
+        int[] result = new int[nlen];
+
+        int[] a1 = new int[nlen];
+        int[] a2 = new int[nlen];
+        a1[0] = 1;
+        a2[nlen-1] = 1;
+
+        for(int i = 0 ; i<nlen ;i++){
+            if(i<nlen-1) {
+                a1[i + 1] = a1[i] * nums[i];
+            }
+
+
+
+        }
+
+        for(int i = 0 ; i<nlen ;i++){
+            int j = nlen - 1 - i;
+            if(j-1>=0) {
+                a2[j - 1] = a2[j] * nums[j];
+                result[j-1] = a2[j-1] * a1[j-1];
+            }
+        }
+
+
+        return result;
+    }
+
+
     public static void main(String[] args){
         int[] a = {1,2,3,4};
-        int[] b = productExceptSelf(a);
+        int[] b = productExceptSelf2(a);
         for(int i=0;i<b.length;i++) {
-            System.out.print(b[i]);
+            System.out.println(b[i]);
         }
     }
 
