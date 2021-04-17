cb9c56958d99e114f8036b364cee8eab17fb51da
jinsong <1156083136@qq.com>
2021-04-18 01:57:36 +0800

    chordRandom

---
+++ src/main/java/com/jinsong/lifeapp/GuitarChordRandom.java
@@ -0,0 +1,42 @@
+package com.jinsong.lifeapp;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GuitarChordRandom{
+    public static void main(String[] args) throws  Exception {
+
+        List<String> chords = new ArrayList<>();
+        chords.add("A");
+        chords.add("B");
+        chords.add("C");
+        chords.add("D");
+        chords.add("E");
+        chords.add("F");
+        chords.add("G");
+
+        Integer counter = 1;
+
+        while(1 == 1){
+            int a = (int)(Math.random()*100)%7;
+            String root = chords.get(a);
+
+            int r1 = (int)(Math.random()*100)%2;
+            int r2 = (int)(Math.random()*100)%2;
+
+            if(r1 == 0 && r2 ==0){
+                System.out.print(root);
+            }else if(r1 == 1 && r2 == 1){
+                System.out.print(root+"m");
+            }else if(r1==0&& r2==1){
+                System.out.print(root+"7");
+            }else{
+                System.out.print(root+"m7");
+            }
+            System.out.println("\t\t\t\t"+counter);
+            Thread.sleep(5000);
+            counter++;
+        }
+    }
+}
